6a0b428b13e904b8c1ca4b3cc2316d8737e0046e
Georg Rokita <rokita.georg@freenet.de>
2016-03-09 16:32:05 +0200

    mistake in tempSens

--- Vent-Con-System02/src/TempSens.cpp
+++ Vent-Con-System02/src/TempSens.cpp
@@ -6,56 +6,60 @@
  */
 
 #include "TempSens.h"
-
+#include  <cstdio>
 
 TempSens::TempSens(){
 
-	/* Setup ADC for 12-bit mode and normal power */
-		Chip_ADC_Init(LPC_ADC0, 0);
 
-		/* Setup for maximum ADC clock rate */
-		Chip_ADC_SetClockRate(LPC_ADC0, ADC_MAX_SAMPLE_RATE);
+	Chip_ADC_Init(LPC_ADC0, 0);
 
-		/* For ADC0, sequencer A will be used without threshold events.
-		   It will be triggered manually  */
-		Chip_ADC_SetupSequencer(LPC_ADC0, ADC_SEQA_IDX, (ADC_SEQ_CTRL_CHANSEL(0) | ADC_SEQ_CTRL_CHANSEL(3) | ADC_SEQ_CTRL_MODE_EOS));
+	/* Setup for maximum ADC clock rate */
+	Chip_ADC_SetClockRate(LPC_ADC0, ADC_MAX_SAMPLE_RATE);
 
-		/* For ADC0, select analog input pint for channel 0 on ADC0 */
-		Chip_ADC_SetADC0Input(LPC_ADC0, 0);
+	/* For ADC0, sequencer A will be used without threshold events.
+			   It will be triggered manually  */
+	Chip_ADC_SetupSequencer(LPC_ADC0, ADC_SEQA_IDX, (ADC_SEQ_CTRL_CHANSEL(0) | ADC_SEQ_CTRL_CHANSEL(3) | ADC_SEQ_CTRL_MODE_EOS));
 
-		/* Use higher voltage trim for both ADC */
-		Chip_ADC_SetTrim(LPC_ADC0, ADC_TRIM_VRANGE_HIGHV);
+	/* For ADC0, select analog input pint for channel 0 on ADC0 */
+	Chip_ADC_SetADC0Input(LPC_ADC0, 0);
 
-		/* Assign ADC0_0 to PIO1_8 via SWM (fixed pin) and ADC0_3 to PIO0_5 */
-		Chip_SWM_EnableFixedPin(SWM_FIXED_ADC0_0);
-		Chip_SWM_EnableFixedPin(SWM_FIXED_ADC0_3);
+	/* Use higher voltage trim for both ADC */
+	Chip_ADC_SetTrim(LPC_ADC0, ADC_TRIM_VRANGE_HIGHV);
 
-		/* Need to do a calibration after initialization and trim */
-		Chip_ADC_StartCalibration(LPC_ADC0);
-		while (!(Chip_ADC_IsCalibrationDone(LPC_ADC0))) {}
+	/* Assign ADC0_0 to PIO1_8 via SWM (fixed pin) and ADC0_3 to PIO0_5 */
+	Chip_SWM_EnableFixedPin(SWM_FIXED_ADC0_0);
+	Chip_SWM_EnableFixedPin(SWM_FIXED_ADC0_3);
 
-		/* Clear all pending interrupts and status flags */
-		Chip_ADC_ClearFlags(LPC_ADC0, Chip_ADC_GetFlags(LPC_ADC0));
+	/* Need to do a calibration after initialization and trim */
+	Chip_ADC_StartCalibration(LPC_ADC0);
+	while (!(Chip_ADC_IsCalibrationDone(LPC_ADC0))) {}
 
-		/* Enable sequence A completion interrupts for ADC0 */
-		Chip_ADC_EnableInt(LPC_ADC0, ADC_INTEN_SEQA_ENABLE);
+	/* Clear all pending interrupts and status flags */
+	Chip_ADC_ClearFlags(LPC_ADC0, Chip_ADC_GetFlags(LPC_ADC0));
 
-		/* Enable related ADC NVIC interrupts */
-		NVIC_EnableIRQ(ADC0_SEQA_IRQn);
+	/* Enable sequence A completion interrupts for ADC0 */
+	Chip_ADC_EnableInt(LPC_ADC0, ADC_INTEN_SEQA_ENABLE);
 
-		/* Enable sequencer */
-		Chip_ADC_EnableSequencer(LPC_ADC0, ADC_SEQA_IDX);
+	/* Enable related ADC NVIC interrupts */
+	NVIC_EnableIRQ(ADC0_SEQA_IRQn);
 
-}
+	/* Enable sequencer */
+	Chip_ADC_EnableSequencer(LPC_ADC0, ADC_SEQA_IDX);
 
-TempSens:: ~TempSens(){
 
 }
 
 float TempSens::readValues()
 {
 	Chip_ADC_StartSequencer(LPC_ADC0, ADC_SEQA_IDX);
-	return ADC_DR_RESULT(Chip_ADC_GetDataReg(LPC_ADC0, 0))/100;
+	float val;
+	val = ADC_DR_RESULT(Chip_ADC_GetDataReg(LPC_ADC0, 0));
+	val/=100;
+
+	return val;
+
+
+
 }
 
 
